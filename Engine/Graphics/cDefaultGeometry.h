c08bb09d8bc9854d4f8d82c9e82f79e93f7f0d23
AN Zhu <zhua@kean.edu>
2019-09-19 01:02:21 -0600

    Begin

--- Engine/Graphics/cDefaultGeometry.h
+++ Engine/Graphics/cDefaultGeometry.h
@@ -6,6 +6,7 @@
 #endif
 #include "cVertexFormat.h"
 #include "VertexFormats.h"
+#include <Engine/Assets/ReferenceCountedAssets.h>
 namespace eae6320 {
 	namespace Graphics {
 		struct sDataRequriedToIntializeObject {
@@ -15,13 +16,22 @@ namespace eae6320 {
 			uint16_t * indexdata = nullptr;
 		};
 		class DefaultGeometry {
+			static eae6320::cResult Create(const sDataRequriedToIntializeObject& data, DefaultGeometry* i_instance) {
+				DefaultGeometry* instance = new DefaultGeometry();
+				eae6320::cResult result = instance->InitializeGeometry(data);
+				i_instance = instance;
+				return result;
+			}
 		public:
+			//TODO change these to private
 			DefaultGeometry();
-			DefaultGeometry(const DefaultGeometry& other) = delete;
+			eae6320::cResult InitializeGeometry(const sDataRequriedToIntializeObject& data);
+			eae6320::cResult CleanUp();
+
+			EAE6320_ASSETS_DECLAREREFERENCECOUNTINGFUNCTIONS()
+			EAE6320_ASSETS_DECLAREDELETEDREFERENCECOUNTEDFUNCTIONS(DefaultGeometry)
 			const DefaultGeometry& operator == (const DefaultGeometry& other) = delete;
 			void Draw();
-			eae6320::cResult CleanUp();
-			eae6320::cResult InitializeGeometry(const sDataRequriedToIntializeObject& data);
 			~DefaultGeometry();
 			//Add indexCountToRender and indexbuffer
 			int indexCountToRender = 6;
@@ -30,10 +40,12 @@ namespace eae6320 {
 			GLuint m_vertexArrayId = 0;
 			GLuint m_indexBufferId = 0;
 #elif defined EAE6320_PLATFORM_D3D
+			//The reference count was put at the last place to make the structure smaller
 			eae6320::Graphics::cVertexFormat::Handle m_vertexFormat;
 			ID3D11Buffer* m_vertexBuffer = nullptr;
 			ID3D11Buffer* m_indexBuffer = nullptr;
 #endif
+			EAE6320_ASSETS_DECLAREREFERENCECOUNT()
 		};
 	}
 }
