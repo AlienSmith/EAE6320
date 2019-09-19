1059dcf3980e8663ad559aa9d64776c815895e91
AN Zhu <zhua@kean.edu>
2019-09-19 01:05:51 -0600

    Still Works

--- Engine/Graphics/cEffect.h
+++ Engine/Graphics/cEffect.h
@@ -23,18 +23,27 @@ namespace eae6320
 		};
 		class cEffect {
 		public:
+			static eae6320::cResult Create(const sDataRequriedToIntializeEffect& data, cEffect* i_instance) {
+				cEffect* instance = new cEffect();
+				eae6320::cResult result = instance->InitializeShadingData(data);
+				i_instance = instance;
+				return result;
+			}
+			EAE6320_ASSETS_DECLAREREFERENCECOUNTINGFUNCTIONS()
+			EAE6320_ASSETS_DECLAREDELETEDREFERENCECOUNTEDFUNCTIONS(cEffect)
 			eae6320::Graphics::cShader::Handle m_vertexShader;
 			eae6320::Graphics::cShader::Handle m_fragmentShader;
 			eae6320::Graphics::cRenderState::Handle m_renderState;
 #ifdef EAE6320_PLATFORM_GL 
 			GLuint m_programId;
 #endif
+			EAE6320_ASSETS_DECLAREREFERENCECOUNT()
 			void Bind();
 			cResult CommonCleanUp();
 			cResult commonInitializeShadingData(const sDataRequriedToIntializeEffect& data);
 			cResult CleanUp();
 			cResult InitializeShadingData(const sDataRequriedToIntializeEffect& data);
-			cEffect() {}
+			cEffect(){}
 			virtual ~cEffect() {}
 		};
 		class cBEffect {
