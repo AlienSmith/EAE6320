80de342b36fe0a5dedbc8588bc47372718ed37b5
AN Zhu <zhua@kean.edu>
2019-10-03 03:57:22 -0600

    commit

--- MyGame_/Content/Geometry/Triangle.lua
+++ MyGame_/Content/Geometry/Triangle.lua
@@ -1,4 +1,9 @@
 return{
-	VertexData = {{0.0,0.0,0.0},{1.0,0.0,0.0},{1.0,1.0,0.0}},
+	-- Triangle
+	-- VertexCount = 3
+	VertexData = {{0.0,0.0,0.0},
+				  {1.0,0.0,0.0},
+				  {1.0,1.0,0.0}},
+	-- IndexCount = 3
 	IndexData  = {0,1,2},
 }
