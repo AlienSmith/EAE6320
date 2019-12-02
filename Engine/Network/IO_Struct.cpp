9206c169f8f0b7ec7074e725bbd4fb464fc3705c
AN Zhu <zhua@kean.edu>
2019-12-02 16:22:40 -0600

    Commit

--- Engine/Network/IO_Struct.cpp
+++ Engine/Network/IO_Struct.cpp
@@ -1,10 +1,10 @@
 #include "IO_Struct.h"
 #include <iostream>
 #include <thread>
-Network::InputStruct* Network::ServerLogic::GetInputStructure()
-{
-	return m_input_data;
-}
+//Network::InputStruct* Network::ServerLogic::GetInputStructure()
+//{
+//	return m_input_data;
+//}
 
 Network::UpdateStruct* Network::ServerLogic::GetUpdateStructure()
 {
@@ -29,12 +29,17 @@ void Network::ServerLogic::Update()
 	else {
 		delta_time = (float)difftime(current_time, m_update_structure.last_time);
 	}
-	m_update_structure.speed[0] = eae6320::Math::sVector((float)(*m_ptr_inputs)[0].input_x_axies, (float)(*m_ptr_inputs)[0].input_y_axies, 0.0f);
-	m_update_structure.speed[1] = eae6320::Math::sVector((float)(*m_ptr_inputs)[1].input_x_axies, (float)(*m_ptr_inputs)[1].input_y_axies, 0.0f);
-	m_update_structure.speed[0] *= 0.5f;
-	m_update_structure.speed[1] *= 0.5f;
-	m_update_structure.position[0] += m_update_structure.speed[0] * 0.1f;
-	m_update_structure.position[1] += m_update_structure.speed[1] * 0.1f;
+	for (int i = 0; i < MAX_CLIENT_NUMBER; i++) {
+		m_update_structure.speed[i] = eae6320::Math::sVector((float)(*m_ptr_inputs)[i].input_x_axies, (float)(*m_ptr_inputs)[i].input_y_axies, 0.0f);
+		m_update_structure.speed[i] *= 0.5f;
+		m_update_structure.position[i] += m_update_structure.speed[i] * 0.01f * (float)(i+1);
+	}
+	//m_update_structure.speed[0] = eae6320::Math::sVector((float)(*m_ptr_inputs)[0].input_x_axies, (float)(*m_ptr_inputs)[0].input_y_axies, 0.0f);
+	//m_update_structure.speed[0] *= 0.5f;
+	//m_update_structure.position[0] += m_update_structure.speed[0] * 0.1f;
+	//m_update_structure.speed[1] *= 0.5f;
+	//m_update_structure.position[1] += m_update_structure.speed[1] * -0.2f;
+	//m_update_structure.speed[1] = eae6320::Math::sVector((float)(*m_ptr_inputs)[1].input_x_axies, (float)(*m_ptr_inputs)[1].input_y_axies, 0.0f);
 	m_update_structure.last_time = current_time;
 	return ;
 }
--- Engine/Network/IO_Struct.h
+++ Engine/Network/IO_Struct.h
@@ -8,9 +8,15 @@ namespace Network {
 		int input_y_axies = 0;
 	};
 	struct UpdateStruct {
-		eae6320::Math::sVector position[MAX_CLIENT_NUMBER]        = { eae6320::Math::sVector(0.0f,0.0f,0.0f),eae6320::Math::sVector(0.0f,0.0f,0.0f) };
-		eae6320::Math::sVector speed[MAX_CLIENT_NUMBER]       = { eae6320::Math::sVector(0.0f,0.0f,0.0f),eae6320::Math::sVector(0.0f,0.0f,0.0f) };
+		eae6320::Math::sVector position[MAX_CLIENT_NUMBER];
+		eae6320::Math::sVector speed[MAX_CLIENT_NUMBER];
 		time_t last_time = 0;
+		UpdateStruct() {
+			for (int i = 0; i < MAX_CLIENT_NUMBER; i++) {
+				position[i] = eae6320::Math::sVector(0.0f, 0.0f, 0.0f);
+				speed[i] = eae6320::Math::sVector(0.0f, 0.0f, 0.0f);
+			}
+		}
 	};
 	class ClientLogic {
 	public:
@@ -24,13 +30,14 @@ namespace Network {
 	};
 	class ServerLogic {
 	public:
-		InputStruct* GetInputStructure();
+		//InputStruct* GetInputStructure();
 		UpdateStruct* GetUpdateStructure();
 		void SetInputStructPtr(const InputStruct(*InputStruct)[MAX_CLIENT_NUMBER]);
 		void Update();
 	private:
-		InputStruct m_input_data[MAX_CLIENT_NUMBER];
-		const InputStruct(*m_ptr_inputs)[MAX_CLIENT_NUMBER] = &m_input_data;
+		//InputStruct m_input_data[MAX_CLIENT_NUMBER];
+		//const InputStruct(*m_ptr_inputs)[MAX_CLIENT_NUMBER] = &m_input_data;
+		const InputStruct(*m_ptr_inputs)[MAX_CLIENT_NUMBER] = nullptr;
 		UpdateStruct m_update_structure;
 	};
 }
